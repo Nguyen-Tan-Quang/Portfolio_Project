2aece23306e7db6e5a34b1d4c6aebc0dc7ae5a94
Nguyen-Tan-Quang <2054032342quang@gmail.com>
2023-08-05 21:20:06 +0700

    Fix sql-rfm

--- SQL_RFM/SQL-RFM.sql
+++ SQL_RFM/SQL-RFM.sql
@@ -1,84 +1,84 @@
 ---Inspecting Data
-select * from [PortfolioProject].[dbo].[sales_data_sample]
+SELECT * FROM [PortfolioProject].[dbo].[sales_data]
 
 --CHecking unique values
-select distinct status from [PortfolioProject].[dbo].[sales_data_sample] --Nice one to plot
-select distinct year_id from [PortfolioProject].[dbo].[sales_data_sample]
-select distinct PRODUCTLINE from [PortfolioProject].[dbo].[sales_data_sample] ---Nice to plot
-select distinct COUNTRY from [PortfolioProject].[dbo].[sales_data_sample] ---Nice to plot
-select distinct DEALSIZE from [PortfolioProject].[dbo].[sales_data_sample] ---Nice to plot
-select distinct TERRITORY from [PortfolioProject].[dbo].[sales_data_sample] ---Nice to plot
+SELECT DISTINCT status FROM [PortfolioProject].[dbo].[sales_data] --Nice one to plot
+SELECT DISTINCT year_id FROM [PortfolioProject].[dbo].[sales_data]
+SELECT DISTINCT PRODUCTLINE FROM [PortfolioProject].[dbo].[sales_data] ---Nice to plot
+SELECT DISTINCT COUNTRY FROM [PortfolioProject].[dbo].[sales_data] ---Nice to plot
+SELECT DISTINCT DEALSIZE FROM [PortfolioProject].[dbo].[sales_data] ---Nice to plot
+SELECT DISTINCT TERRITORY FROM [PortfolioProject].[dbo].[sales_data] ---Nice to plot
 
-select distinct MONTH_ID from [PortfolioProject].[dbo].[sales_data_sample]
-where year_id = 2003
+SELECT DISTINCT MONTH_ID FROM [PortfolioProject].[dbo].[sales_data]
+WHERE year_id = 2003
 
 ---ANALYSIS
 ----Let's start by grouping sales by productline
-select PRODUCTLINE, sum(sales) Revenue
-from [PortfolioProject].[dbo].[sales_data_sample]
-group by PRODUCTLINE
-order by 2 desc
+SELECT PRODUCTLINE, SUM(sales) Revenue
+FROM [PortfolioProject].[dbo].[sales_data]
+GROUP BY PRODUCTLINE
+ORDER BY 2 DESC
 
-select YEAR_ID, sum(sales) Revenue
-from [PortfolioProject].[dbo].[sales_data_sample]
-group by YEAR_ID
-order by 2 desc
+SELECT YEAR_ID, SUM(sales) Revenue
+FROM [PortfolioProject].[dbo].[sales_data]
+GROUP BY YEAR_ID
+ORDER BY 2 DESC
 
-select  DEALSIZE,  sum(sales) Revenue
-from [PortfolioProject].[dbo].[sales_data_sample]
-group by  DEALSIZE
-order by 2 desc
+SELECT  DEALSIZE,  SUM(sales) Revenue
+FROM [PortfolioProject].[dbo].[sales_data]
+GROUP BY  DEALSIZE
+ORDER BY 2 DESC
 
 ----What was the best month for sales in a specific year? How much was earned that month? 
-select  MONTH_ID, sum(sales) Revenue, count(ORDERNUMBER) Frequency
-from [PortfolioProject].[dbo].[sales_data_sample]
-where YEAR_ID = 2004 --change year to see the rest
-group by  MONTH_ID
-order by 2 desc
+SELECT  MONTH_ID, SUM(sales) Revenue, COUNT(ORDERNUMBER) Frequency
+FROM [PortfolioProject].[dbo].[sales_data]
+WHERE YEAR_ID = 2004 --change year to see the rest
+GROUP BY  MONTH_ID
+ORDER BY 2 DESC
 
 --November seems to be the month, what product do they sell in November, Classic I believe
-select  MONTH_ID, PRODUCTLINE, sum(sales) Revenue, count(ORDERNUMBER)Frequency
-from [PortfolioProject].[dbo].[sales_data_sample]
-where YEAR_ID = 2004 
-and MONTH_ID = 11 --change year to see the rest
-group by  MONTH_ID, PRODUCTLINE
-order by 3 desc
+SELECT  MONTH_ID, PRODUCTLINE, SUM(sales) Revenue, COUNT(ORDERNUMBER)Frequency
+FROM [PortfolioProject].[dbo].[sales_data]
+WHERE YEAR_ID = 2004 
+AND MONTH_ID = 11 --change year to see the rest
+GROUP BY  MONTH_ID, PRODUCTLINE
+ORDER BY 3 DESC
 
 DROP TABLE IF EXISTS #rfm
-;with rfm as 
+;WITH rfm AS 
 (
-	select 
+	SELECT 
 		CUSTOMERNAME, 
-		sum(sales) MonetaryValue,
-		avg(sales) AvgMonetaryValue,
-		count(ORDERNUMBER) Frequency,
-		max(ORDERDATE) last_order_date,
-		(select max(ORDERDATE) from [PortfolioProject].[dbo].[sales_data_sample]) max_order_date,
-		DATEDIFF(DD, max(ORDERDATE), (select max(ORDERDATE) from [PortfolioProject].[dbo].[sales_data_sample])) Recency
-	from [PortfolioProject].[dbo].[sales_data_sample]
-	group by CUSTOMERNAME
-),rfm_calc as
+		SUM(sales) MonetaryValue,
+		AVG(sales) AvgMonetaryValue,
+		COUNT(ORDERNUMBER) Frequency,
+		MAX(ORDERDATE) last_order_date,
+		(SELECT MAX(ORDERDATE) FROM [PortfolioProject].[dbo].[sales_data]) max_order_date,
+		DATEDIFF(DD, MAX(ORDERDATE), (SELECT MAX(ORDERDATE) FROM [PortfolioProject].[dbo].[sales_data])) Recency
+	FROM [PortfolioProject].[dbo].[sales_data]
+	GROUP BY CUSTOMERNAME
+),rfm_calc AS
 (
 
-	select r.*,
-		NTILE(4) OVER (order by Recency desc) rfm_recency,
-		NTILE(4) OVER (order by Frequency) rfm_frequency,
-		NTILE(4) OVER (order by MonetaryValue) rfm_monetary
-	from rfm r
-)select 
-	c.*, rfm_recency+ rfm_frequency+ rfm_monetary as rfm_cell,
-	cast(rfm_recency as varchar) + cast(rfm_frequency as varchar) + cast(rfm_monetary  as varchar)rfm_cell_string
+	SELECT r.*,
+		NTILE(4) OVER (ORDER BY Recency DESC) rfm_recency,
+		NTILE(4) OVER (ORDER BY Frequency) rfm_frequency,
+		NTILE(4) OVER (ORDER BY MonetaryValue) rfm_monetary
+	FROM rfm r
+)SELECT 
+	c.*, rfm_recency+ rfm_frequency+ rfm_monetary AS rfm_cell,
+	CAST(rfm_recency AS varchar) + CAST(rfm_frequency AS varchar) + CAST(rfm_monetary  AS varchar)rfm_cell_string
 into #rfm
-from rfm_calc c
+FROM rfm_calc c
 
-select CUSTOMERNAME , rfm_recency, rfm_frequency, rfm_monetary,
-	case 
-		when rfm_cell_string in (111, 112 , 121, 122, 123, 132, 211, 212, 114, 141) then 'lost_customers'  --lost customers
-		when rfm_cell_string in (133, 134, 143, 244, 334, 343, 344, 144) then 'slipping away, cannot lose' -- (Big spenders who haven’t purchased lately) slipping away
-		when rfm_cell_string in (311, 411, 331) then 'new customers'
-		when rfm_cell_string in (222, 223, 233, 322) then 'potential churners'
-		when rfm_cell_string in (323, 333,321, 422, 332, 432) then 'active' --(Customers who buy often & recently, but at low price points)
-		when rfm_cell_string in (433, 434, 443, 444) then 'loyal'
-		else 'not_segmented'
-	end as rfm_segment
-from #rfm
+SELECT CUSTOMERNAME , rfm_recency, rfm_frequency, rfm_monetary,
+	CASE 
+		WHEN rfm_cell_string in (111, 112 , 121, 122, 123, 132, 211, 212, 114, 141) THEN 'lost_customers'  --lost customers
+		WHEN rfm_cell_string in (133, 134, 143, 244, 334, 343, 344, 144) THEN 'slipping away, cannot lose' -- (Big spenders who haven’t purchased lately) slipping away
+		WHEN rfm_cell_string in (311, 411, 331) THEN 'new customers'
+		WHEN rfm_cell_string in (222, 223, 233, 322) THEN 'potential churners'
+		WHEN rfm_cell_string in (323, 333,321, 422, 332, 432) THEN 'active' --(Customers who buy often & recently, but at low price points)
+		WHEN rfm_cell_string in (433, 434, 443, 444) THEN 'loyal'
+		ELSE 'not_segmented'
+	END AS rfm_segment
+FROM #rfm
